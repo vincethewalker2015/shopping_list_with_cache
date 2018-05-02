6e481aa6cf7ae235d77b46fe2384b187a0a0726b
vincentbwalker <vincethewalker@gmail.com>
2018-05-02 15:38:40 +0000

    Added Index, Show, edit functions

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -17,6 +17,32 @@ class ShoppingsController < ApplicationController
     @shop = Shopping.find(params[:id])
   end
   
+  def edit
+    @shop = Shopping.find(params[:id])
+    
+  end
+  
+  def update
+    @shop = Shopping.find(params[:id])
+    if @shop.update(shopping_params)
+      flash[:notice] = "Item was sucessfully Updated"
+      redirect_to shopping_path(@shop)
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @shop = Shopping.all
+  end
+  
+  def destroy
+    @shop = Shopping.find(params[:id])
+    @shop.destroy
+    flash[:notice] = "Deleted"
+    redirect_to shoppings_path
+  end
+  
   
   private
   
